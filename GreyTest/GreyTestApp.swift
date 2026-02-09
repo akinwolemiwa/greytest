4ffbb8f688df60a35b3482825c797364d4c56d97
akinwolemiwa <akin.siyanbola@gmail.com>
2026-02-09 23:02:46 +0100

    Initial Commit

---
+++ GreyTest/GreyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreyTestApp.swift
+//  GreyTest
+//
+//  Created by AKIN on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GreyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
